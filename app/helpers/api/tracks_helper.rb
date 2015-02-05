8dd431ecfaa4fdd378aa61ff42e116c95e65c9f9
peterjeliot <peterjeliot@gmail.com>
2015-02-05 11:12:04 -0800

    Fix namespacing for other Track things

--- app/helpers/api/tracks_helper.rb
+++ app/helpers/api/tracks_helper.rb
@@ -1,2 +1,2 @@
-module TracksHelper
+module Api::TracksHelper
 end
--- test/controllers/api/tracks_controller_test.rb
+++ test/controllers/api/tracks_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class TracksControllerTest < ActionController::TestCase
+class Api::TracksControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
