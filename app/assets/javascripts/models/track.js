a4e129357948989ae7f8e11324018c2305891c67
peterjeliot <peterjeliot@gmail.com>
2015-02-09 15:31:41 -0800

    Inline player tracks position of song

--- app/assets/javascripts/models/track.js
+++ app/assets/javascripts/models/track.js
@@ -12,10 +12,21 @@ SoundPile.Models.Track = Backbone.Model.extend({
     if (payload.audio_url) {
       this.audio = soundManager.createSound({
         id: payload.id,
-        url: payload.audio_url
+        url: payload.audio_url,
+        whileplaying: function (audio) {
+          this.trigger("position:change");
+        }.bind(this),
       });
-      console.log(this.audio);
     }
     return payload;
+  },
+
+  position: function () {
+    return this.audio.position;
+  },
+
+  duration: function () {
+    //TODO: Return this in the JSON?
+    return this.audio.durationEstimate;
   },
 });
--- app/assets/javascripts/views/player/inline_player.js
+++ app/assets/javascripts/views/player/inline_player.js
@@ -6,13 +6,19 @@ SoundPile.Views.InlinePlayer = Backbone.CompositeView.extend({
   },
 
   initialize: function () {
-    this.listenTo(this.model, 'progress:playing', this.render);
+    this.listenTo(this.model, 'position:change', this.renderProgress);
   },
 
   render: function () {
     var track = this.model;
     this.$el.html(this.template({ track: this.model }));
     return this;
+  },
+
+  renderProgress: function () {
+    var fractionalPosition = this.model.position() / this.model.duration();
+    var percentPosition = (fractionalPosition * 100) + "%";
+    this.$(".position-bar").css("width", percentPosition);
   },
 
   play: function (event) {
